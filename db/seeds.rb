5078af2206a6fb1a41a0ffbada72557780216893
YiJunKim <ybc864@naver.com>
2016-05-10 11:36:43 +0000

    Post 테이블의 시드 데이터 입력하기

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,18 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = ["한식", "일식", "중식", "양식"]
+
+categories.each do |category|
+	0.upto(1) do |i|
+		p = Post.new
+		p.user_id = i + 1
+		p.category = category
+		p.title = "#{category}집#{i} 이름"
+		p.content = "#{category}집#{i} 설명"
+		p.save
+	end
+end
+
