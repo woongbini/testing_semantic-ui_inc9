91eaf9e8bccbd95cb0b69cfbba4a70b22c8fff8b
Woong Bin Kim <gamebook1004@gmail.com>
2016-05-12 09:10:28 +0000

    게시글과 댓글의 작성자 정보 추가하기

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -22,6 +22,7 @@ class FoodsController < ApplicationController
 
   def show
 		@post = Post.find(params[:id])
+		@comment_writer = User.where(id: session[:user_id])[0]
   end
 
   def write
@@ -29,6 +30,7 @@ class FoodsController < ApplicationController
 
   def write_complete
 		post = Post.new
+		post.user_id = session[:user_id]
 		post.category = params[:post_category]
 		post.title = params[:post_title]
 		post.content = params[:post_content]
@@ -68,6 +70,7 @@ class FoodsController < ApplicationController
 
 	def write_comment_complete
 		comment = Comment.new
+		comment.user_id = session[:user_id]
 		comment.post_id = params[:post_id]
 		comment.content = params[:comment_content]
 		comment.save
